afb03d630e809e6f231afe6aed0643175d7d71a4
chandrushekar <chandru@chandrushekars-MacBook-Air.local>
2022-07-28 21:47:42 +0530

    Authorization feature changes

--- app/api/routers/health_check_router.py
+++ app/api/routers/health_check_router.py
@@ -1,17 +1,37 @@
 import logging
-from fastapi import APIRouter
+from typing import Callable
+from fastapi import APIRouter, Request, Response
+from fastapi.routing import APIRoute
+
 from api.models.health_check_status import HealthCheckStatus
+
 
 logger = logging.getLogger(__name__)
 
+
+class LogRoute(APIRoute):
+    def get_route_handler(self) -> Callable:
+        original_route_handler = super().get_route_handler()
+
+        async def custom_route_handler(request: Request) -> Response:
+            req = request.method, request.url
+            logger.info(req)
+            response: Response = await original_route_handler(request)
+            resp = response.body, response.status_code
+            logger.info(resp)
+            return response
+
+        return custom_route_handler
+
+
 router = APIRouter(
-    prefix='/social_api'
+    route_class=LogRoute,
+    prefix='/social_api',
+    tags=['healthCheck']
 )
 
 
 @router.get('/health_check', response_model=HealthCheckStatus)
 def health_check():
-    logger.info('Health check performance')
-
+    logger.info('Server Health Check')
     return HealthCheckStatus(is_server_running=True)
-
--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 import uvicorn
 
-from api.routers import health_check_router
+from api.routers import health_check_router, iss_position_router, login_router
 from logger_config import setup_log_config
 
 setup_log_config()
@@ -9,6 +9,9 @@ setup_log_config()
 app = FastAPI()
 
 app.include_router(health_check_router.router)
+app.include_router(login_router.router)
+app.include_router(iss_position_router.router)
+
 
 if __name__ == '__main__':
     uvicorn.run(app, host='0.0.0.0', port=8080)
